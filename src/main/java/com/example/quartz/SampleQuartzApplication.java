2b8c140c9256c82d9fbe21d60e4e6a014bd6c6ff
Bajal <mohiyudeen.bajal@nytimes.com>
2020-07-06 21:05:14 -0400

    take out schedule to prop file

--- src/main/java/com/example/quartz/SampleQuartzApplication.java
+++ src/main/java/com/example/quartz/SampleQuartzApplication.java
@@ -2,8 +2,11 @@ package com.example.quartz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:schedules.properties")
 public class SampleQuartzApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/quartz/job/ConfigureJob.java
+++ src/main/java/com/example/quartz/job/ConfigureJob.java
@@ -1,11 +1,18 @@
 package com.example.quartz.job;
 
 import org.quartz.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigureJob {
+
+    @Value("${chaseJobCronExpression}")
+    private String chaseJobCronExpression;
+
+    @Value("${amexJobCronExpression}")
+    private String amexJobCronExpression;
 
     @Bean
     public JobDetail chaseJobDetails() {
@@ -14,17 +21,20 @@ public class ConfigureJob {
     }
 
     @Bean
-    public Trigger jobATrigger(JobDetail chaseJobDetails) {
+    public Trigger chaseJobTrigger(JobDetail chaseJobDetails) {
         return TriggerBuilder.newTrigger().forJob(chaseJobDetails)
                 .withIdentity("ChaseTrigger")
-                .withSchedule(CronScheduleBuilder.cronSchedule("0 * * ? * * *")) //Every 30 secs
+                .withSchedule(CronScheduleBuilder.cronSchedule(chaseJobCronExpression)) //Every 30 secs
                 .build();
     }
 
-
     @Bean
     public JobDetail amexJobDetails() {
+        // Set Retry Limit
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put("retries", 3);
         return JobBuilder.newJob(AmexJob.class).withIdentity("AmexJob")
+                .usingJobData(jobDataMap)
                 .storeDurably().build();
     }
 
@@ -32,7 +42,7 @@ public class ConfigureJob {
     public Trigger amexJobTrigger(JobDetail amexJobDetails) {
         return TriggerBuilder.newTrigger().forJob(amexJobDetails)
                 .withIdentity("AmexTrigger")
-                .withSchedule(CronScheduleBuilder.cronSchedule("0 * * ? * * *")) //Every minute
+                .withSchedule(CronScheduleBuilder.cronSchedule(amexJobCronExpression)) //Every minute
                 .build();
     }
 
